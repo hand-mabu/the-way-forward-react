6d769ef2c3d79b00206f42f5e919f768cfc9b182
linlinlin123456 <CLtramp@outlook.com>
2019-03-03 10:43:25 +0800

    add

--- exercise/cl/clock/src/App.js
+++ exercise/cl/clock/src/App.js
@@ -2,46 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import MyHeader from "./component/MyHeader";
 import MyFoot from "./component/MyFoot";
-import MySeconds from "./component/MySeconds";
-import MyButtons from "./component/MyButtons";
-import MyRecord from "./component/MyRecord";
-
+import StopWatch from "./component/StopWatch/StopWatch";
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      // 显示在页面的秒数
-      timedif: "00:00.00",
-      // 计次数组
-      count: []
-    };
-  }
-  sendSecond(timedif) {
-    this.setState({ timedif: timedif });
-  }
-  sendCount(count) {
-    console.log("sendCount");
-    if (count == "null") {
-      this.setState({ count: [] });
-    } else {
-      this.setState({ count: [...this.state.count, count] });
-    }
-  }
-
   render() {
     return (
       <div className="App">
         <div className="container">
           <MyHeader />
-          <div className="top">
-            <MySeconds timedif={this.state.timedif} />
-            <MyButtons
-              seconds={this.sendSecond.bind(this)}
-              timedif={this.state.timedif}
-              count={this.sendCount.bind(this)}
-            />
-          </div>
-          <MyRecord count={this.state.count} />
+          <StopWatch />
           <MyFoot />
         </div>
       </div>
